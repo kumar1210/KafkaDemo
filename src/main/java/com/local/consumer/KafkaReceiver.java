f7a743edb3a18725983478ac68db7ade8855f734
gaurav's <gaurav's@champ>
2018-07-19 12:08:42 +0530

    commit 2

--- src/main/java/com/local/consumer/KafkaReceiver.java
+++ src/main/java/com/local/consumer/KafkaReceiver.java
@@ -3,6 +3,8 @@
  */
 package com.local.consumer;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -15,9 +17,16 @@ public class KafkaReceiver {
 
 	private static final Logger LOG = LoggerFactory.getLogger(KafkaReceiver.class);
 	
+	private CountDownLatch latch = new CountDownLatch(1);
+	
+	public CountDownLatch getLatch() {
+	    return latch;
+	}
+	
 	@KafkaListener(topics = "${kafka.topic.name}")
 	public void consume(String message) {
 		
 		LOG.info("Message received is '{}'", message);
+		latch.countDown();
 	}
 }
